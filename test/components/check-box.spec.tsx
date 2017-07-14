cb7302f42d73ad8722a20ba2998f91494b97b66d
liorzisman <liorzisman@gmail.com>
2017-07-14 17:54:54 +0300

    more refactoring done

--- test/components/check-box.spec.tsx
+++ test/components/check-box.spec.tsx
@@ -1,57 +1,41 @@
 import React = require('react');
 
-import { expect, selectDom, waitForDom, sinon, waitFor, simulate } from 'test-drive-react';
+import { expect, sinon, waitFor, simulate, ClientRenderer } from 'test-drive-react';
 import { CheckBox } from '../../src/components';
 import { Automation } from '../../src/common/automation';
 
-const ReactDOM = require('react-dom');
-
 describe('<CheckBox />', () => {
-  let app, select;
 
-  before(() => {
-    app = document.createElement('div');
-    app.id = 'app';
-    document.body.appendChild(app);
-    select = selectDom(app);
-  });
+  const clientRenderer = new ClientRenderer();
 
   afterEach(() => {
-    const newApp = document.createElement('div');
-    newApp.id = 'app';
-    document.body.replaceChild(newApp, app);
-    app = newApp;
-    select = selectDom(app);
-  });
-
-  after(() => {
-    document.body.removeChild(app);
+    clientRenderer.cleanup();
   });
 
   it('is rendered to the screen', () => {
-    ReactDOM.render(<CheckBox value={true} onChange={() => console.log('hi')}/>, app);
+    const { select, waitForDom } = clientRenderer.render(<CheckBox value={true} onChange={() => {}} />);
     const checkbox = select(Automation.CHECK_BOX);
 
-    return waitForDom(app, () => expect(checkbox).to.be.present());
+    return waitForDom(() => expect(checkbox).to.be.present());
   });
 
   it('is set to \'Checked\' when the value is true', () => {
-    ReactDOM.render(<CheckBox value={true} onChange={() => console.log('hi')}/>, app);
-    const checkbox = select(Automation.CHECK_BOX);
+    const { select, waitForDom } = clientRenderer.render(<CheckBox value={true} onChange={() => {}} />);
+    const checkbox = select(Automation.CHECK_BOX) as HTMLInputElement;
 
-    return waitForDom(app, () => expect(checkbox.checked).to.eql(true));
+    return waitForDom(() => expect(checkbox.checked).to.equal(true));
   });
 
-  it('is set to \'Unchecked\' when the value is true', () => {
-    ReactDOM.render(<CheckBox value={false} onChange={() => console.log('hi')}/>, app);
-    const checkbox = select(Automation.CHECK_BOX);
+  it('is set to \'Unchecked\' when the value is false', () => {
+    const { select, waitForDom } = clientRenderer.render(<CheckBox value={false} onChange={() => {}} />);
+    const checkbox = select(Automation.CHECK_BOX) as HTMLInputElement;
 
-    return waitForDom(app, () => expect(checkbox.checked).to.eql(false));
+    return waitForDom(() => expect(checkbox.checked).to.equal(false))
   });
 
   it('invokes he provided onChanged when the checkbox is clicked', () => {
     const onChange = sinon.spy();
-    ReactDOM.render(<CheckBox value={false} onChange={onChange}/>, app);
+    const { select } = clientRenderer.render(<CheckBox value={false} onChange={onChange} />);
 
     simulate.change(select(Automation.CHECK_BOX));
 
