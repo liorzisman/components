73dcbf6a6f8a422a5ad2463c078de128ee2e9baa
liorzisman <liorzisman@gmail.com>
2017-07-14 17:56:55 +0300

    refactored tests

--- test/components/image-link.spec.tsx
+++ test/components/image-link.spec.tsx
@@ -1,41 +1,24 @@
 import React = require('react');
 
-import {expect, selectDom, waitForDom} from 'test-drive-react';
+import { expect, ClientRenderer } from 'test-drive-react';
 import { ImageLink } from '../../src/components';
 import { Automation } from '../../src/common/automation';
-
-const ReactDOM = require('react-dom');
 
 const githubImageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/120px-Octicons-mark-github.svg.png';
 const googleUrl = 'www.google.com';
 
 describe('<ImageLink />', function() {
-  let app, select;
-
-  before(() => {
-    app = document.createElement('div');
-    app.id = 'app';
-    document.body.appendChild(app);
-    select = selectDom(app);
-  });
+  const clientRenderer = new ClientRenderer();
 
   afterEach(() => {
-    const newApp = document.createElement('div');
-    app.id = 'app';
-    document.body.replaceChild(newApp, app);
-    app = newApp;
-    select = selectDom(app);
-  });
-
-  after(() => {
-    document.body.removeChild(app);
+    clientRenderer.cleanup();
   });
 
   it('is rendered to the screen', () => {
-    ReactDOM.render(<ImageLink imageSrc={githubImageUrl} url={googleUrl}/>, app);
+    const { select, waitForDom } = clientRenderer.render(<ImageLink imageSrc={githubImageUrl} url={googleUrl} />);
     const socialLink = select(Automation.IMAGE_LINK);
 
-    return waitForDom(app, () => expect(socialLink).to.be.present());
+    return waitForDom(() => expect(socialLink).to.be.present());
   });
 });
 
--- test/components/text-input.spec.tsx
+++ test/components/text-input.spec.tsx
@@ -1,51 +1,33 @@
 import React = require('react');
 
-import { expect, selectDom, waitForDom, sinon, waitFor, simulate } from 'test-drive-react';
+import { expect, ClientRenderer, sinon, waitFor, simulate } from 'test-drive-react';
 import { TextInput } from '../../src/components';
 import { Automation } from '../../src/common/automation';
 
-const ReactDOM = require('react-dom');
-
 describe('<TextInput />', () => {
-  let app, select;
-
-  before(() => {
-    app = document.createElement('div');
-    app.id = 'app';
-    document.body.appendChild(app);
-    select = selectDom(app);
-  });
+  const clientRenderer = new ClientRenderer();
 
   afterEach(() => {
-    const newApp = document.createElement('div');
-    newApp.id = 'app';
-    document.body.replaceChild(newApp, app);
-    app = newApp;
-    select = selectDom(app);
-  });
-
-  after(() => {
-    document.body.removeChild(app);
+    clientRenderer.cleanup();
   });
 
   it('is rendered to the screen', () => {
-    ReactDOM.render(<TextInput value="Hello" onChange={() => console.log('hi')}/>, app);
+    const { select, waitForDom } = clientRenderer.render(<TextInput value="Hello" onChange={() => {}} />);
     const textInput = select(Automation.TEXT_INPUT);
 
-    return waitForDom(app, () => expect(textInput).to.be.present());
+    return waitForDom(() => expect(textInput).to.be.present());
   });
 
   it('invokes the onChange when text is entered', () => {
     const onChange = sinon.spy();
 
-    ReactDOM.render(<TextInput value="" onChange={onChange}/>, app);
+    const { select } = clientRenderer.render(<TextInput value="" onChange={onChange} />);
 
-    const textInput = select(Automation.TEXT_INPUT, 'NATIVE_INPUT');
+    const textInput = select(Automation.TEXT_INPUT, 'NATIVE_INPUT') as HTMLInputElement;
     textInput.value = 'hello';
     simulate.change(textInput);
 
     return waitFor(() => expect(onChange).to.have.been.calledOnce);
   });
-
 });
 
